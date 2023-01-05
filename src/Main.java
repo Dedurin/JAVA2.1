cea865aded91acf6199725b289e9464989391141
Dedurin <grem85krbr@mail,ru>
2023-01-05 20:04:51 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 2500;
+        int miles = 20;
+        int bonus = ticket / miles;
+        System.out.println(" Бонусные мили - " + bonus);
+
+    }
+}
